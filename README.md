018d2a33cb8daf8071894da6179491ebd9b69ede
Guilhormes <guilhermeciadope@gmail.com>
2023-03-18 02:05:39 -0300

    Update - responsividade do mobile

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Descrição
 
-Arlindo Barbearia é um projeto feito com o objetivo de melhorar habilidades de Front-end sem nenhum fim lucrativo ou com intenção de roubar layouts e ideias de outros sites oficiais.
+Arlindo Barbearia é um projeto feito em grupo com o objetivo de melhorar habilidades de Front-end sem nenhum fim lucrativo ou com intenção de roubar layouts e ideias de outros sites oficiais.
 O Site está sendo desenvolvido utilizando tecnologias: HTML, CSS e JS(Javascript).
--- js/mobile-navbar.js
+++ js/mobile-navbar.js
@@ -1,47 +1,47 @@
-class MobileNavbar{
+ class MobileNavbar{
     constructor(mobileMenu, navList, navLinks) {
         this.mobileMenu = document.querySelector(mobileMenu);
         this.navList = document.querySelector(navList);
         this.navLinks = document.querySelectorAll(navLinks);
-        this.activeClass = "active";
+         this.activeClass = "active";
 
         this.handleClick = this.handleClick.bind(this);
-    }
+     }
 
 
-    animateLinks() {
-        this.navLinks.forEach((link, index) => {
+     animateLinks() {
+         this.navLinks.forEach((link, index) => {
           link.style.animation
-            ? (link.style.animation = "")
-            : (link.style.animation = `navLinkFade 0.5s ease forwards ${
+             ? (link.style.animation = "")
+             : (link.style.animation = `navLinkFade 0.5s ease forwards ${
                 index / 7 + 0.3
-              }s`);
+             }s`);
         });
-      }
+       }
 
     handleClick(){
-        this.navList.classList.toggle(this.activeClass);
-        this.mobileMenu.classList.toggle(this.activeClass);
+         this.navList.classList.toggle(this.activeClass);
+         this.mobileMenu.classList.toggle(this.activeClass);
         this.animateLinks();
-        }
+         }
 
 
-    addClickEvent() {
-        this.mobileMenu.addEventListener("click", this.handleClick);
-    }
+     addClickEvent() {
+         this.mobileMenu.addEventListener("click", this.handleClick);
+     }
 
-    init() {
-        if (this.mobileMenu) {
-            this.addClickEvent();
-        }
-        return this;
+     init() {
+         if (this.mobileMenu) {
+             this.addClickEvent();
+         }
+         return this;
     }
 }
 
-const mobileNavbar = new MobileNavbar(
-    ".mobile-menu",
-    ".nav-ul",
-    ".nav-ul li",
-  );
+ const mobileNavbar = new MobileNavbar(
+     ".mobile-menu",
+     ".nav-ul",
+     ".nav-ul li",
+   );
 
-  mobileNavbar.init();
+   mobileNavbar.init();
